9f9f6458f100ed06bf4f7e2261379f81c9c3f8e8
Galbadrakh Battur <b.galbadrah12@gmail.com>
2019-08-06 23:57:55 +0800

    test

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ app.post('/api/courses/', (req, res) => {
 
     if(error) return res.status(400).send(error.details[0].message);
         
+
+    
     
     const course = {
         id: courses.length+1,
